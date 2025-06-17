ea103322003c1246828b0552e7807c4d4b1d9dd8
Kiril Ivanov <ki7867831@gmail.com>
2025-06-17 20:31:13 +0300

    Add .gitignore and exclude .vs folder

---
+++ EventHubApp.Data/EventHubAppDbContext.cs
@@ -0,0 +1,12 @@
+﻿namespace EventHubApp.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+    public class EventHubAppDbContext : IdentityDbContext
+    {
+        public EventHubAppDbContext(DbContextOptions<EventHubAppDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
