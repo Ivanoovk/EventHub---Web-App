41e393d8907382a1b64441aaf9e4d77fc8b6379a
Ivanoovk <ki78ki7867831@gmail.com>
2025-07-12 15:29:37 +0300

    new implementation added

--- EventHubApp.GCommon/ApplicationConstants.cs
+++ EventHubApp.GCommon/ApplicationConstants.cs
@@ -6,5 +6,9 @@ namespace EventHubApp.GCommon
     {
         public const string AppDateFormat = "yyyy-MM-dd";
         public const string NoImageUrl = "no-image.jpg";
+        public const string IsDeletedPropertyName = "IsDeleted";
+        public const string PriceSqlType = "decimal(18, 6)";
+        public const string AccessDeniedPath = "/Home/AccessDenied";
+        public const string ManagerAuthCookie = "ManagerAuth";
     }
 }
