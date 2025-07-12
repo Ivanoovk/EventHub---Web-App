655cdef2214e81991498172d2247a15c3e12e4f4
Ivanoovk <ki78ki7867831@gmail.com>
2025-07-12 15:30:43 +0300

    new implementation added

---
+++ EventHubApp.Web.ViewModels/Event/EventFormInputModel.cs
@@ -0,0 +1,49 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+using static EventHubApp.GCommon.ApplicationConstants;
+using static EventHubApp.Web.ViewModels.ValidationMessages.Event;
+using static EventHubApp.Data.Common.EntityConstants.Event;
+
+namespace EventHubApp.Web.ViewModels.Event
+{
+    public class EventFormInputModel
+    {
+        // Id will be validated in the corresponding Service method
+        public string Id { get; set; }
+            = string.Empty;
+
+        [Required(ErrorMessage = TitleRequiredMessage)]
+        [MinLength(TitleMinLength, ErrorMessage = TitleMinLengthMessage)]
+        [MaxLength(TitleMaxLength, ErrorMessage = TitleMaxLengthMessage)]
+        public string Title { get; set; } = null!;
+
+        [Required(ErrorMessage = TypeRequiredMessage)]
+        [MinLength(TypeMinLength, ErrorMessage = TypeMinLengthMessage)]
+        [MaxLength(TypeMaxLength, ErrorMessage = TypeMaxLengthMessage)]
+        public string Type { get; set; } = null!;
+
+        [Required(ErrorMessage = ReleaseDateRequiredMessage)]
+        public string ReleaseDate { get; set; } = null!;
+
+        [Required(ErrorMessage = DurationRequiredMessage)]
+        [Range(DurationMin, DurationMax, ErrorMessage = DurationRangeMessage)]
+        public int Duration { get; set; }
+
+        [Required(ErrorMessage = SponsorRequiredMessage)]
+        [MinLength(SponsorNameMinLength, ErrorMessage = SponsorNameMinLengthMessage)]
+        [MaxLength(SponsorNameMaxLength, ErrorMessage = SponsorNameMaxLengthMessage)]
+        public string Sponsor { get; set; } = null!;
+
+        [Required(ErrorMessage = DescriptionRequiredMessage)]
+        [MinLength(DescriptionMinLength, ErrorMessage = DescriptionMinLengthMessage)]
+        [MaxLength(DescriptionMaxLength, ErrorMessage = DescriptionMaxLengthMessage)]
+        public string Description { get; set; } = null!;
+
+        [MaxLength(ImageUrlMaxLength, ErrorMessage = ImageUrlMaxLengthMessage)]
+        public string? ImageUrl { get; set; }
+            = $"/images/{NoImageUrl}";
+    }
+}
+
