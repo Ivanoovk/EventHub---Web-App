944041afd036e09d59f1a10451ec776a2c5ed070
Ivanoovk <ki78ki7867831@gmail.com>
2025-08-03 00:06:20 +0300

    new implementation added

---
+++ EventHubApp.Web.Infrastructure/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,41 @@
+﻿
+
+using EventHubApp.Data.Seeding.Interfaces;
+using EventHubApp.Web.Infrastructure.Middlewares;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EventHubApp.Web.Infrastructure.Extensions
+{
+    public static class WebApplicationExtensions
+    {
+        public static IApplicationBuilder UseManagerAccessRestriction(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ManagerAccessRestrictionMiddleware>();
+
+            return app;
+        }
+
+        public static IApplicationBuilder UserAdminRedirection(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<AdminRedirectionMiddleware>();
+
+            return app;
+        }
+
+        public static IApplicationBuilder SeedDefaultIdentity(this IApplicationBuilder app)
+        {
+            using IServiceScope scope = app.ApplicationServices.CreateScope();
+            IServiceProvider serviceProvider = scope.ServiceProvider;
+
+            IIdentitySeeder identitySeeder = serviceProvider
+                .GetRequiredService<IIdentitySeeder>();
+            identitySeeder
+                .SeedIdentityAsync()
+                .GetAwaiter()
+                .GetResult();
+
+            return app;
+        }
+    }
+}
